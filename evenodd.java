9df5f4b18bee9cd1091b298ecc0820d6d7fe3e94
anshika1303jain-star <anshika1303jain@gmail.com>
2026-02-10 10:28:14 +0530

    Organized code into DSA folder

--- evenodd.java
+++ evenodd.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
-public class evenodd{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+public class evenodd {
+   public evenodd() {
+   }
 
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+   public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      System.out.print("Enter a number: ");
+      int var2 = var1.nextInt();
+      if (var2 % 2 == 0) {
+         System.out.println("Even number");
+      } else {
+         System.out.println("Odd number");
+      }
 
-        if (num % 2 == 0) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
-        }
-
-        sc.close();
-    }
+      var1.close();
+   }
 }
